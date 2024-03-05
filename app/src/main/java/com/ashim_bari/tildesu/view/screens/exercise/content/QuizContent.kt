04bf7ba201a401e3db663b742fdf4b9ee78d66a1
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-03-05 15:10:10 +0500

    Minor updates

--- app/src/main/java/com/ashim_bari/tildesu/view/screens/exercise/content/QuizContent.kt
+++ app/src/main/java/com/ashim_bari/tildesu/view/screens/exercise/content/QuizContent.kt
@@ -1,13 +1,15 @@
 package com.ashim_bari.tildesu.view.screens.exercise.content
 
 import android.util.Log
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -17,6 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,6 +34,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -88,18 +92,31 @@ fun QuizContent(navController: NavController, level: String, type: ExerciseType,
                     modifier = Modifier
                         .padding(16.dp)
                         .fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
                 ) {
+                    LinearProgressIndicator(
+                        progress = { currentQuestionIndex.toFloat() / exercises.size },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
                     if (exercises.isNotEmpty() && currentQuestionIndex < exercises.size) {
                         val currentExercise = exercises[currentQuestionIndex]
-                        Text(text = currentExercise.question ?: "No question available", style = MaterialTheme.typography.bodyMedium, modifier = Modifier.padding(bottom = 8.dp))
-
+                        Text(
+                            text = " ${currentExercise.question ?: "No question available"}",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.padding(16.dp)
+                        )
                         LazyVerticalGrid(
                             columns = GridCells.Fixed(2),
                             contentPadding = PaddingValues(8.dp),
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            modifier = Modifier.fillMaxWidth()
                         ) {
                             currentExercise.options?.let {
                                 items(it.size) { index ->
@@ -170,24 +187,32 @@ fun QuizContent(navController: NavController, level: String, type: ExerciseType,
 
 
 @Composable
-fun OptionCard(option: String, isSelected: Boolean, modifier: Modifier = Modifier, onSelect: () -> Unit) {
+fun OptionCard(option: String, isSelected: Boolean, onSelect: () -> Unit) {
+    // Assign distinct colors to each option
+    val backgroundColor = when (option) {
+        "What" -> Color(0xFFFFCDD2) // Example color - replace with actual colors you want
+        "Whose" -> Color(0xFFC8E6C9)
+        "How" -> Color(0xFFFFECB3)
+        "Where" -> Color(0xFFB3E5FC)
+        else -> MaterialTheme.colorScheme.surfaceVariant
+    }
+
     Card(
-        modifier = modifier
-            .padding(vertical = 4.dp)
-            .clickable(onClick = onSelect)
-            .aspectRatio(1f), // Optional: Makes the card square-shaped
-        shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surfaceVariant
-        )
+        onClick = onSelect,
+        modifier = Modifier
+            .padding(8.dp)
+            .aspectRatio(1f)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surfaceVariant)
     ) {
-        Text(
-            text = option,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.CenterHorizontally)
-        )
+        Box(contentAlignment = Alignment.Center) { // Use Box with contentAlignment
+            Text(
+                text = option,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
